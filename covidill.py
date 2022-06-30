24a51444db56e380634033ec550bda974fe3d400
i-inose <izuru.03042002@gmail.com>
2022-06-30 19:39:10 +0900

    更新

--- covidill.py
+++ covidill.py
@@ -5,19 +5,15 @@ import numpy as np
 import datetime
 
 #前処理1
-df = pd.read_csv("domestic_daily.csv")
+df = pd.read_csv("domestic.csv")
 index = pd.date_range("2020-05-09", "2022-05-28", freq = "D")
 df.index = index
-df = df.drop(df.columns[2:], axis=1)
-df = df.drop(df.columns[0], axis=1)
-df = df.rename(columns={"ALL" : "Number of seriously injured"})
+df = df.iloc[:,1]
 #前処理２
-df1 = pd.read_csv("domestic_daily.csv")
+df1 = pd.read_csv("deaths.csv")
 index1 = pd.date_range("2020-05-09", "2022-05-28", freq = "D")
 df1.index = index1
-df1 = df1.drop(df1.columns[2:], axis=1)
-df1 = df1.drop(df1.columns[0], axis=1)
-df1 = df1.rename(columns={"ALL" : "Number of deaths"})
+df1 = df1.iloc[:, 3]
 
 #作図
 plt.figure(figsize= (15, 9))
@@ -28,7 +24,7 @@ dmax = "2022-05-28"
 xmin = datetime.datetime.strptime(dmin, "%Y-%m-%d")
 xmax = datetime.datetime.strptime(dmax, "%Y-%m-%d")
 plt.xlim([xmin,xmax])
-plt.ylim([0, 2300])
+plt.ylim([0, 300])
 plt.grid(True)
 plt.plot(df, color="red", label="injured")
 plt.plot(df1, color="blue", label="deaths")
